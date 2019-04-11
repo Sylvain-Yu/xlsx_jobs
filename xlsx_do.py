dfe432439d92c820fb13916dbfe99829021c5baa
Sylvain-Yu <jasonshake@163.com>
2019-04-11 16:06:33 +0800

    delete useless file

--- xlsx_do.py
+++
@@ -1,8 +0,0 @@
-from openpyxl import load_workbook
-wb = load_workbook("./MAGELEC_TestData/1000rpm_290V_Mot.xlsx")
-
-current_sheet = wb["Instantly Data"]
-
-for i in range(2,30):
-    data = current_sheet["E"+str(i)].value
-    print(data)
