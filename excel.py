68ed0312b3fbae4435b89d58183354e4dfec5543
Sylvain-Yu <jasonshake@163.com>
2019-04-19 14:09:57 +0800

    improve BEMF

--- excel.py
+++ excel.py
@@ -10,15 +10,20 @@ class Excel():  # read and write the xlsx and process.
         self.wb = load_workbook(self.filename)
         self.current_sheet = self.wb[self.sheetname]
 
-    def WriteBEMF(self):
-        print("正在处理反电动势数据中，请等待...")
+    def Read_One_Value(self,single_pos):
+        value = self.current_sheet[single_pos].value
+        return value
+
+    def save(self):
+        self.wb.save(self.filename)
+        print("完成数据处理\n")
+
+    def WriteBEMF_value(self):
+        i = 0
+        j = 0
+
         for v in self.Dict_temp["MB_Command.Speed"]:
-            speed_range = [1000,2000,3000,4000]
-            try:
-                i = speed_range.index(v)
-            except ValueError:
-                status = "BEMF 数据处理失败 !\n"
-            else:
+            if self.target_speed == v :
                 position = self.listforposition[i]+"14"
                 self.current_sheet[position] = self.Dict_temp["U-RMS.Voltage"][i]
                 position = self.listforposition[i]+"15"
@@ -31,13 +36,22 @@ class Excel():  # read and write the xlsx and process.
                 self.current_sheet[position] = self.Dict_temp["V-PP.RMS.Voltage"][i]
                 position = self.listforposition[i]+"22"
                 self.current_sheet[position] = self.Dict_temp["W-PP.RMS.Voltage"][i]
-                status = "BEMF 数据处理完成 !\n"
-        self.wb.save(self.filename)
-        print(status)
-
-    def Read_One_Value(self,single_pos):
-        value = self.current_sheet[single_pos].value
-        return value
+                j += 1
+            i += 1
+        if j == 0:
+            print("没有找到转速为%s的数据\n"%self.target_speed)
+        else:
+            print("已找到转速为%s,找到%s个，已选取最后一组数据"%(self.target_speed,j))
+            
+            
+    def WriteBEMF(self):
+        print("正在处理反电动势数据中，请等待...")
+        listforNum = range(len(self.listforposition))
+        SpeedPositionList = [ x + "9" for x in self.listforposition ]
+        for self.j ,Speed_pos in zip(listforNum,SpeedPositionList):
+            self.target_speed = self.Read_One_Value(Speed_pos)
+            self.WriteBEMF_value()
+        self.save()
 
 #仅写入耐久的数据
     def WriteConti_value(self):
@@ -72,14 +86,13 @@ class Excel():  # read and write the xlsx and process.
                 self.current_sheet[position] = self.Dict_temp["MA-RTD 1"][i]
                 position = self.listforposition[self.j]+"23"
                 self.current_sheet[position] = self.Dict_temp["MA-RTD 2"][i]
-
-
                 j += 1
             i += 1
         if j == 0:
             print("没有找到转速为%s的数据\n"%self.Conti_Speed)
         else:
             print("已找到转速为%s,找到%s个，已选取最后一组数据"%(self.Conti_Speed,j))
+
 # 连续扭矩试验处理数据，属于源为Instantly
     def WriteConti(self):
         print("正在处理连续扭矩数据中，请等待...")
@@ -138,9 +151,7 @@ class Excel():  # read and write the xlsx and process.
         else:
             print("已找到转速为%s,找到%s个，已选取最后一组数据"%(self.target_speed,j))
 
-    def save(self):
-        self.wb.save(self.filename)
-        print("完成数据处理\n")
+
 
     def WriteSc(self):
         print("正在处理短路电流数据中，请等待...")
